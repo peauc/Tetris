b54fd0413dc19e6006e5d7d53a0803683cab6787
thoma_c <clement.thomas@epitech.eu>
2016-03-04 16:01:34 +0100

    push aff

---
+++ src/aff_next.c
@@ -0,0 +1,18 @@
+/*
+** aff_next.c for aff_next in /home/thoma_c/rendu/PSU_2015_tetris/src
+**
+** Made by Clement Thomas
+** Login   <thoma_c@epitech.net>
+**
+** Started on  Fri Mar  4 14:56:48 2016 Clement Thomas
+** Last update Fri Mar  4 15:41:39 2016 Clement Thomas
+*/
+
+#include "tetris.h"
+
+int	screen_size(WINDOW *origin)
+{
+  getmaxy(origin);
+  getmaxy(origin);
+  return (0);
+}
