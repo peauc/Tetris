a9435e88b9c0aaa47b73521d79c85a7820d52c19
thoma_c <clement.thomas@epitech.eu>
2016-03-12 15:48:02 +0100

    ...

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 /*
 ** main.c for main in /home/thoma_c/rendu/PSU_2015_tetris/src
 **
@@ -5,30 +6,27 @@
 ** Login   <thoma_c@epitech.net>
 **
 ** Started on  Wed Feb 24 15:30:25 2016 Clement Thomas
-** Last update Mon Mar  7 18:32:07 2016 Clement Peau
+** Last update Fri Mar 11 15:23:04 2016 Clement Thomas
 */
 
 #include "tetris.h"
 
-int		main(int argc, char **argv)
+int             main(int argc, char **argv)
 {
   printf("REMETRE LE WERROR AVANT DE PUSH ET LE TETRIS\n");
-  t_game	game;
-  /* WINDOW	*win; */
-  /* t_board	board; */
+  t_game        game;
+  char          **keys;
+  WINDOW        *win;
+  t_board       board;
 
-  /* win = initscr(); */
   /* argc = argc; */
   /* argv = argv; */
   game.list = parsing();
-  options(&game, argv);
-  /* data_board_initialize(&board); */
-  /* aff_tetris(0, new, &board, win); */
-  /* srand(time(NULL)); */
-  /* new = newterm(NULL, stderr, stdin); */
-  /* new = set_term(new); */
-  /* keys = options(list, argv); */
-  /* data_board_initialize(&board, game.list); */
-  /* aff_tetris(0, &board, game.list); */
+  data_board_initialize(&board, game.list, &game);
+  /* options(&game, argv); */
+  /* keys = options(game.list, argv); */
+  srand(time(NULL));
+  if ((aff_tetris(0, &board, game.list, &game)) == -1)
+    write(2, "The screen size must be highter\n", 32);
   return (0);
 }
